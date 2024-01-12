4fb9a6794ae516ec8c8fc94cb06d0c4e3298afa9
Dokky <kld9223@kakao.com>
2024-01-12 19:29:43 +0900

    test
    1

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform objectToFollow; // 따라갈 오브젝트
+    public float followSpeed = 10f; // 따라갈 스피드
+    public float sensitivity = 500f; // 감도
+    public float clampAngle = 70f; // 카메라 뒤집히는거 방지 각도
+
+    private float rotX;
+    private float rotY;
+
+    public Transform realCamera; // 카메라 정보
+    public Vector3 dirNormalized;
+    public Vector3 finalDir; // 최종방향
+    public float minDistance; // 최소거리
+    public float maxDistance; // 최대거리
+    public float finalDinstance; // 최종거리
+    public float smoothness = 10f;
+
+
+    private void Start()
+    {
+        // 시작 시 인풋 초기화
+        rotX = transform.localRotation.eulerAngles.x;
+        rotY = transform.localRotation.eulerAngles.y;
+
+        dirNormalized = realCamera.localPosition.normalized;
+        finalDinstance = realCamera.localPosition.magnitude; //magniture라는건 크기
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        rotX += -(Input.GetAxis("Mouse Y")) * sensitivity * Time.deltaTime; // 현재프레임에서 마지막프레임 간격시간
+        rotY += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+        Quaternion rot = Quaternion.Euler(rotX, rotY, 0);
+        transform.rotation = rot;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position
+            = Vector3.MoveTowards(transform.position, objectToFollow.position, followSpeed * Time.deltaTime);
+
+        finalDir = transform.TransformPoint(dirNormalized * maxDistance);
+
+        RaycastHit hit;
+
+        if (Physics.Linecast(transform.position, finalDir, out hit))
+        {
+            finalDinstance = Mathf.Clamp(hit.distance, minDistance, maxDistance);
+        }
+        else
+        {
+            finalDinstance = maxDistance;
+        }
+        realCamera.localPosition
+            = Vector3.Lerp(realCamera.localPosition, dirNormalized * finalDinstance, Time.deltaTime * smoothness);
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform characterBody;
+    [SerializeField]
+    private Transform cameraArm;
+    Animator animator;
+
+
+    void Start()
+    {
+        animator = characterBody.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    void FixedUpdate()
+    {
+       // Move();
+
+    }
+
+   
+    // 이동 방향 구하기
+    private void Move()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isMove = moveInput.magnitude != 0;
+        bool isRun = Input.GetKey(KeyCode.LeftShift);
+
+        animator.SetBool("isMove", isMove);
+        animator.SetBool("isRun", isRun);
+
+        if (isMove)
+        {
+            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+
+            characterBody.transform.forward = moveDir * Time.deltaTime;
+
+            if (isRun)
+            {
+                transform.position += Vector3.ClampMagnitude(moveDir, 1f) * Time.deltaTime * 8f;
+            }
+            else
+            {
+                transform.position += Vector3.ClampMagnitude(moveDir, 1f) * Time.deltaTime * 5f;
+            }
+
+        }
+
+
+    }
+
+}
